16336536eb678f8aa0e5db8af067c1dd1e01739a
NVG-064 <aricandra064@student.esaunggul.ac.id>
2023-06-02 21:34:59 +0700

    Completed exercise 5 and renamed package

--- src/main/kotlin/id/infinitelearning/kotlin_submission/exercise5/Hero.kt
+++ src/main/kotlin/id/infinitelearning/kotlin_submission/exercise5/Hero.kt
@@ -1,56 +1,85 @@
-package id.infinitelearning.KotlinSubmission.exercise5
+package id.infinitelearning.kotlin_submission.exercise5
 
 class Hero {
 
-    private var name: String? = null
-    private var age = 0
-    private var height = 0
+  private var name: String? = null
+  private var age = 0
+  private var height = 0
 
-    private var health = 10
+  private var health = 10
 
-    fun setProfile(name: String?, age: Int, height: Int) {
-        this.name = name
-        this.age = age
-        this.height = height
+  fun setProfile(name: String?, age: Int, height: Int) {
+    this.name = name
+    this.age = age
+    this.height = height
+  }
+
+  fun profile() {
+    println("========= Profile =========")
+    println("Name   : $name")
+    println("Age    : $age")
+    println("Height : $height")
+    println("Health : $health")
+    println("===========================\n")
+  }
+
+  private fun checkHealth() {
+    if (health <= 0) {
+      println("Anda telah mati $name\n")
     }
+  }
 
-    fun profile() {
-        println("========= Profile =========")
-        println("Name: $name")
-        println("Age: $age")
-        println("Height: $height")
-        println("Health: $health")
-        println("===========================")
-    }
+  fun jalan() {
+    checkHealth()
+    println("$name sedang berjalan ... *kesandung batu\n")
+    health -= 1
+  }
 
-    private fun checkHealth() {
-        if (health <= 0) {
-            println("Anda telah mati $name")
-        }
-    }
+  fun lari() {
+    checkHealth()
+    println("$name lagi lari pagi ... lagi binsik (bina fisik) orangnya ...\n")
+    health -= 2
+  }
 
-    fun jalan() {
-        checkHealth()
-        println("$name sedang berjalan...")
-        health -= 1
-    }
+  fun makan() {
+    checkHealth()
+    println("$name lagi makan ... *nyam nyam\n")
+    health += 2
+  }
+
+  fun minum() {
+    checkHealth()
+    println("$name lagi minum ... *gluk gluk gluk\n")
+    health += 2
+  }
+
+  fun lompat() {
+    checkHealth()
+    println("$name lagi lompat ... *hap\n")
+    health -= 2
+  }
+
+  fun duduk() {
+    checkHealth()
+    println("$name lagi duduk ... *tapi resah karena lagi ambeien ...\n")
+    health += 1
+  }
 
 
+  /**
+   * Buat fungsi untuk lari, makan, minum, lompat, dan duduk. Masing2 memiliki kriteria sebagai berikut:
+  - pasang fungsi checkHealth() di dalam fungsi yang akan dibuat
+  - Tampilkan text kegiatan yang sedang dilakukan, misal, jika lari: "$name sedang berlari..."
+  - ubah nilai variable health sesuai kegiatan yang dilakukan:
+   * lari = -2
+   * makan = +2
+   * minum = +2
+   * lompat = -2
+   * duduk = +1
 
-    /**
-     * Buat fungsi untuk lari, makan, minum, lompat, dan duduk. Masing2 memiliki kriteria sebagai berikut:
-     - pasang fungsi checkHealth() di dalam fungsi yang akan dibuat
-     - Tampilkan text kegiatan yang sedang dilakukan, misal, jika lari: "$name sedang berlari..."
-     - ubah nilai variable health sesuai kegiatan yang dilakukan:
-      * lari = -2
-      * makan = +2
-      * minum = +2
-      * lompat = -2
-      * duduk = +1
+  Contoh fungsi bisa mengikuti fungsi jalan() diatas.
 
-      Contoh fungsi bisa mengikuti fungsi jalan() diatas.
-
-    */
+   */
 
 
 }
